1b36aa9e1b8165de41b30cfcdc14d2f1a48f6fbd
fy0d <72026103+fy0d@users.noreply.github.com>
2020-11-12 23:03:36 +0300

    Update task-4.py

--- task-4.py
+++ task-4.py
@@ -1,12 +1,12 @@
 
 n = int(input('Введите число: '))
-max = n % 10
+max_ = n % 10
 
 while n >= 1:
     n = n // 10
-    if n % 10 > max:
-        max = n % 10
+    if n % 10 > max_:
+        max_ = n % 10
 
-print(max)
+print(max_)
 
 input('\n\nНажмите Enter, чтобы выйти')
